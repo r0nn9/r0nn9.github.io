e4d729189457bcf79d5c436a36915a4503ec27be
Aaron <anajeramolina@hcc-nd,edu>
2026-03-24 09:09:08 -0400

    create startgame

--- hello.js
+++ hello.js
@@ -1,2 +1,24 @@
 let user = prompt("What is your name?");
-console.log("Hello, "+user+"!");
+startgame(user);
+
+function startgame(user)   {
+    console.log("Hi, "+user+". Do you want to play?");
+    console.log("1. Yes");
+    console.log("2. No");
+    console.log("");
+
+    let choice = prompt("Please Choose:");
+    switch (choice) {
+        case "1":
+            careerPath(user);
+            return;
+        case "2":
+            console.log("You choose not to play so bye.")
+            endgame();
+            return;
+        default:
+            console.log(choice+" is not valid. Please enter a number corresponding to your choice.");
+            startgame(user);
+    }
+
+}
